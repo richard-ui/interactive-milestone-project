a0b0dc2254d09cf6aa23757ab8baed41d59027f1
richard-ui <richard-jones96@hotmail.co.uk>
2020-12-06 20:54:11 +0000

    js: Add function to center the map div with specific coordinates

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,32 +1,34 @@
     function initMap(){
-        var map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 10,
+        var map = new google.maps.Map(document.getElementById("map"), { // center map
+            zoom: 11,
             center: {
-               lat: 53.46559,
+               lat: 53.465590,
                 lng: -2.348213
                }
     });
          
-    var labels =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var labels =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // array of labels for markers
 
     
     // array of locations
     
     var locations = [
 
-        ['<h1 class="info_Heading" class="firstHeading">Big Chief</h1>' +
+        ['<h1 class="info_Heading">Big Chief</h1>' +
         '<div id="bodyContent">' +
         '<img width="130" height="100" align="left" src="assets/images/big_chief.jpg" alt="big chief" style="margin-right: 10px;"><b>Contact: </b>07565 341181' +
-        '<div><button type="button" class="btn btn-primary">Contact</button></div>', 53.465590, -2.348213, 4],
+        '<div><button type="button" id="chief" onclick="Choice(this);" class="btn btn-primary">Contact</button></div>', 53.465590, -2.348213, 4],
 
 
         ['<h1 class="info_Heading">Knights Town</h1>' +
             '<div id="bodyContent">' +
-            '<img width="130"  height="100" align="left" src="assets/images/knights_town.jpg" alt="Knights Town" style="margin-right: 10px;"><b>Contact: </b>07565 341181', 53.477314, -2.254962, 5],
+            '<img width="130"  height="100" align="left" src="assets/images/knights_town.jpg" alt="Knights Town" style="margin-right: 10px;"><b>Contact: </b>07565 341181' +
+            '<div><button type="button" id="knight" onclick="Choice(this);" class="btn btn-primary">Contact</button></div>', 53.477314, -2.254962, 5],
 
        ['<h1 class="info_Heading">Lex Deux</h1>' +
         '<div id="bodyContent">' +
-        '<img width="130" height="100" align="left" src="assets/images/les_deux.jpg" alt="Lex Deux" style="margin-right: 10px;"><b>Contact: </b>07565 341181' , 53.446622, -2.308713, 3]
+        '<img width="130" height="100" align="left" src="assets/images/les_deux.jpg" alt="Lex Deux" style="margin-right: 10px;"><b>Contact: </b>07565 341181' +
+        '<div><button type="button" id="lesDeux" onclick="Choice(this);" class="btn btn-primary">Contact</button></div>', 53.446622, -2.308713, 3]
 
     ];
 
@@ -36,7 +38,9 @@
 
     for (i = 0; i < locations.length; i++) {
         marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]), map: map
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+            label: labels[i % labels.length]
         });
 
         google.maps.event.addListener(marker, 'mouseover', (function(marker, i) { // click event listener for infoWindow
@@ -48,8 +52,31 @@
     }
 
 
-    var markerClusterer = new MarkerClusterer(map, markers, 
+    var markerClusterer = new MarkerClusterer(map, markers, // group close markers together
     {imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",});
 
     }
+
+
+function Choice(elem) {
+
+   var box = document.getElementById("box");
+   var restaurant = document.getElementById("restaurant");
+
+
+   if (elem.id == "chief") {
+
+     document.getElementById("divCheckbox").style.display = 'block';
+    // box.style.backgroundColor = "purple";
+
+   } else if (elem.id == "lesDeux") {
+
+     box.style.backgroundColor = "red";
+
+   }else if (elem.id == "knight") {
+
+     box.style.backgroundColor = "green";
+
+   };
+ };
   
