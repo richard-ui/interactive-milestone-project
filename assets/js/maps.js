137a246efbe6ef251fcdbfee114eb91e9201e157
richard-ui <richard-jones96@hotmail.co.uk>
2020-12-10 18:09:26 +0000

    js: set Locations array variable 'Globally' so it can be accessed between multiple functions

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,37 +1,40 @@
     
+    var locations;   // set locations array as global variable, this will be used between 2 functions
+
     function initMap(){
-        var map = new google.maps.Map(document.getElementById("map"), { // center map
+        var map = new google.maps.Map(document.getElementById("map"), {   // center map
             zoom: 11,
             center: {
                lat: 53.465590,
-                lng: -2.348213
+              lng: -2.348213
                }
     });
          
-    var labels =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // array of labels for markers
+    var labels =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";   // array of labels for markers
 
     
     // array of locations
     
-    var locations = [
+    locations = [
 
         ['<h1 class="info_Heading">Big Chief</h1>' +
         '<div id="bodyContent">' +
-        '<img width="130" height="100" align="left" src="assets/images/big_chief.jpg" alt="big chief" style="margin-right: 10px;"><b>Contact: </b>07565 341181' +
-        '<div><button type="button" id="chief" onclick="Choice(this);" class="btn btn-primary">Contact</button></div>', 53.465590, -2.348213, 4],
+        '<img width="130" height="100" align="left" src="assets/images/big_chief.jpg" alt="big chief" style="margin-right: 10px;"><b>Contact: </b>07543 326141' +
+        '<div><button type="button" id="chief" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.465590, -2.348213, 1, "Big Chief"],
 
 
         ['<h1 class="info_Heading">Knights Town</h1>' +
             '<div id="bodyContent">' +
-            '<img width="130"  height="100" align="left" src="assets/images/knights_town.jpg" alt="Knights Town" style="margin-right: 10px;"><b>Contact: </b>07565 341181' +
-            '<div><button type="button" id="knight" onclick="Choice(this);" class="btn btn-primary">Contact</button></div>', 53.477314, -2.254962, 5],
+            '<img width="130"  height="100" align="left" src="assets/images/knights_town.jpg" alt="Knights Town" style="margin-right: 10px;"><b>Contact: </b>07465 342181' +
+            '<div><button type="button" id="knight" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.477314, -2.254962, 2, "Knights Town"],
 
        ['<h1 class="info_Heading">Lex Deux</h1>' +
         '<div id="bodyContent">' +
-        '<img width="130" height="100" align="left" src="assets/images/les_deux.jpg" alt="Lex Deux" style="margin-right: 10px;"><b>Contact: </b>07565 341181' +
-        '<div><button type="button" id="lesDeux" onclick="Choice(this);" class="btn btn-primary">Contact</button></div>', 53.446622, -2.308713, 3]
+        '<img width="130" height="100" align="left" src="assets/images/les_deux.jpg" alt="Lex Deux" style="margin-right: 10px;"><b>Contact: </b>07265 241386' +
+        '<div><button type="button" id="lexDeux" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.446622, -2.308713, 3, "Lex Deux"]
 
     ];
+
 
     var infowindow = new google.maps.InfoWindow // declare info window
 
@@ -51,34 +54,24 @@
             }
         })(marker, i));
     }
-
-
-    var markerClusterer = new MarkerClusterer(map, markers, // group close markers together
-    {imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",});
-
-    }
+}
 
 
 function Choice(elem) {
 
-   var box = document.getElementById("box");
-   var restaurant = document.getElementById("restaurant");
-   
-   document.getElementById("booking-title").innerHTML = "";
+    var restName = ["Restaurant Name: "];
 
    if (elem.id == "chief") {
 
-     document.getElementById("circle").style.display = "block";
-     document.getElementById("booking-title").innerHTML = "Big Chief";
+     document.getElementById("booking-title").textContent = restName[0] + locations[0][4];
 
-   } else if (elem.id == "lesDeux") {
-
-     box.style.backgroundColor = "red";
-     document.getElementById("booking-title").innerHTML = "Lex Deux";
+   } else if (elem.id == "lexDeux") {
+       
+     document.getElementById("booking-title").textContent = restName[0] + locations[1][4];
 
    }else if (elem.id == "knight") {
 
-     box.style.backgroundColor = "green";
+     document.getElementById("booking-title").textContent = restName[0] + locations[2][4];
 
    };
  };
