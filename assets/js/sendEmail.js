606dc07a033c66fc3c696cb26b1dcc10d208c2c7
richard-ui <richard-jones96@hotmail.co.uk>
2020-12-26 16:27:44 +0000

    js: apply validation to form by using jquery if/else statments to check if textboxes are empty. If so error messages will display in labels.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,5 +1,32 @@
 
 function sendMail(contactForm) {
+
+    $("#f_name_label").html("");
+    $("#l_name_label").html("");
+    $("#phone_label").html("");
+    $("#email_label").html("");
+    $("#date_label").html("");
+    $("#hour_label").html("");
+
+    if (!$('#f_name').val()) {
+      $("#f_name_label").html("Please Enter your First Name!");
+    }
+    else if (!$('#l_name').val()) {
+      $("#l_name_label").html("Please Enter your Last Name!");
+    }
+    else if (!$('#phone').val()) {
+      $("#phone_label").html("Please Enter your Phone Number!");
+    }
+    else if (!$('#email').val()) {
+      $("#email_label").html("Please Enter your E-mail!");
+    }
+    else if (!$('#date').val()) {
+      $("#date_label").html("Please Enter a Date!");
+    }
+    else if (!$('#hour').val()) {
+      $("#hour_label").html("Please Enter a Time!");
+    }
+    else{
     emailjs.send("service_csslju6", "Rick", {
         "reserve_name": contactForm.reserve_name.value,
         "first_name": contactForm.f_name.value,
@@ -21,5 +48,6 @@ function sendMail(contactForm) {
             $("#success-form").html("E-mail Error!");
         }
     );
+    }
     return false;  // To block from loading a new page
 }
