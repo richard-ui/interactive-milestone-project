2d1b597e0283e8355d64e6803d4746b48ee58a7a
richard-ui <richard-jones96@hotmail.co.uk>
2020-12-31 15:28:21 +0000

    js: Implement JavaScript function that handles and displays specific City markers when they are selected from the drop down list.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,21 +1,25 @@
-    
-    var locations;   // set locations array as global variable, this will be used between 2 functions
+var locations = [];
+var locations2 = [];
+var locations3 = [];
+var map;
+var markers = []; // array with pushed markers inside
 
-    function initMap(){
-        var map = new google.maps.Map(document.getElementById("map"), {   // center map
+var marker, i;
+
+var lat1 = 53.417375;
+var lng2 = -2.924687;
+
+function initMap(lat1, lng2) {
+  map = new google.maps.Map(document.getElementById("map"), {   // center map
             zoom: 11,
-            center: {
-               lat: 53.465590,
-               lng: -2.348213
-               }
+            center: { lat: lat1, lng: lng2 }
     });
          
     var labels =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";   // array of labels for markers
 
+  // multiple array of locations for cities
     
-    // array of locations
-    
-    locations = [
+    locations1 = [   // MANCHESTER
 
         ['<h1 class="info_Heading">Big Chief</h1>' +
         '<div id="bodyContent">' +
@@ -31,80 +35,183 @@
         ['<h1 class="info_Heading">Knights Town</h1>' +
             '<div id="bodyContent">' +
             '<img width="130"  height="100" align="left" src="assets/images/knights_town.jpg" alt="knights town" style="margin-right: 10px;"><b>Contact: </b>07465 342181' +
-            '<div><button type="button" id="knight" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.477314, -2.254962, 3, "Knights Town"],
+            '<div><button type="button" id="knight" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.477314, -2.254962, 1, "Knights Town", 10],
 
         ['<h1 class="info_Heading">Black Star</h1>' +
             '<div id="bodyContent">' +
             '<img width="130"  height="100" align="left" src="assets/images/black_star.jpg" alt="black star" style="margin-right: 10px;"><b>Contact: </b>07465 322141' +
-            '<div><button type="button" id="bk-star" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.451003, -2.203978, 4, "Black Star"],
+            '<div><button type="button" id="bk-star" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.451003, -2.203978, 12, "Black Star", 10]
 
-        ['<h1 class="info_Heading">El Porto</h1>' +
+    ];
+
+    locations2 = [ // LIVERPOOL
+
+        ['<h1 class="info_Heading">Benson Lodge</h1>' +
+        '<div id="bodyContent">' +
+        '<img width="130" height="100" align="left" src="assets/images/Benson_Lodge.jpg" alt="benson"" style="margin-right: 10px;"><b>Contact: </b>07533 326141' +
+        '<div><button type="button" id="benson" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.417375, -2.924687, 1, "Benson Lodge", 10],
+
+         ['<h1 class="info_Heading">El Porto</h1>' +
             '<div id="bodyContent">' +
             '<img width="130"  height="100" align="left" src="assets/images/el_Porto.jpg" alt="el_porto" style="margin-right: 10px;"><b>Contact: </b>07465 782141' +
-            '<div><button type="button" id="el-porto" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.490885, -2.180429, 5, "El Porto"],
+            '<div><button type="button" id="el-porto" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.392814, -2.935674, 12, "El Porto", 10],
 
         ['<h1 class="info_Heading">Little Italy</h1>' +
             '<div id="bodyContent">' +
             '<img width="130"  height="100" align="left" src="assets/images/little_italy.jpg" alt="little italy" style="margin-right: 10px;"><b>Contact: </b>07966 785141' +
-            '<div><button type="button" id="lil-italy" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.429871, -2.223866, 6, "Little Italy"]
+            '<div><button type="button" id="lil-italy" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.423512, -2.918507, 2, "Little Italy", 10]
+   
+    ];
+
+    locations3 = [ // BRADFORD
+
+        ['<h1 class="info_Heading">Cafe Chocolat</h1>' +
+        '<div id="bodyContent">' +
+        '<img width="130" height="100" align="left" src="assets/images/cafechocolat.jpg" alt="cafe-chocolat" style="margin-right: 10px;"><b>Contact: </b>07543 333141' +
+        '<div><button type="button" id="cafeChoc" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.820898, -1.743720, 1, "cafe chocolat", 10],
+        
+         ['<h1 class="info_Heading">Dirty Bones</h1>' +
+        '<div id="bodyContent">' +
+        '<img width="130" height="100" align="left" src="assets/images/DirtyBones.jpg" alt="dirtyBones" style="margin-right: 10px;"><b>Contact: </b>07543 326241' +
+        '<div><button type="button" id="dirtybones" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.791704, -1.734107, 1, "Dirty Bones", 10],
+
+        ['<h1 class="info_Heading">Fish Forman</h1>' +
+        '<div id="bodyContent">' +
+        '<img width="130" height="100" align="left" src="assets/images/FishForman.jpg" alt="fishforman" style="margin-right: 10px;"><b>Contact: </b>07343 322141' +
+        '<div><button type="button" id="forman" onclick="Choice(this);" class="btn btn-primary info-btn">Contact</button></div>', 53.801640, -1.796248, 1, "Fish Forman", 10]
 
     ];
 
-
-    var infowindow = new google.maps.InfoWindow // declare info window
-
-    var marker, i; // marker variable, i variable to loop through locations
+    var infowindow = new google.maps.InfoWindow(); // declare info window
 
     for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
+
+       marker = new google.maps.Marker({
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
             map: map,
-            label: labels[i % labels.length]
+            label: labels[i % labels.length] // implement labels on markers
         });
 
         google.maps.event.addListener(marker, 'mouseover', (function(marker, i) { // click event listener for infoWindow
             return function() {
                 infowindow.setContent(locations[i][0]); // content from value '0' in array
                 infowindow.open(map, marker);
-            }
+            };
         })(marker, i));
+
+        markers.push(marker); // push current marker into array
+        setMapOnAll(map);
+
     }
+
 }
 
-  // function for info window buttons
+function dropdownfunction(){
+     x = document.getElementById("mySelect");
+
+     item1 = x.options[x.selectedIndex].value; // get selected value from dropdown box
+
+     deleteMarkers();   // run function to delete markers from map
+     locations = [];
+     
+     if(item1 == 'man'){
+       
+     Array.prototype.push.apply(locations, locations1); // add array of locations by city
+     lat1 = 53.465590;
+     lng2 = -2.348213;
+     initMap(lat1, lng2); // run map   
+
+     }
+     else if(item1 == 'liv'){
+
+     Array.prototype.push.apply(locations, locations2); // add array of locations by city
+     lat1 = 53.417375;
+     lng2 = -2.924687;
+     initMap(lat1, lng2); // run map
+     
+     }
+     else if(item1 == 'bra'){
+
+     Array.prototype.push.apply(locations, locations3); // add array of locations by city
+     lat1 = 53.820898;
+     lng2 = -1.743720;
+     initMap(lat1, lng2); // run map
+     
+     }
+ }
+
+function setMapOnAll(map) {                     // Sets all markers in the array to the Map.
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+function deleteMarkers() {      // Deletes all markers in the array by removing references to them.
+  setMapOnAll(null);
+  markers = [];
+}
+
+function bodyOnLoad(){
+    lat1 = 53.465590;
+    lng2 = -2.348213;
+    initMap(lat1, lng2);
+}
+
+// function for info window buttons
 
 function Choice(elem) {
-
+  
     var restName = ["Restaurant Name: "];
     var open_Times = ["Open from ", "pm to ", "pm"];
+    var hour = document.getElementById("hour");
 
    if (elem.id == "chief") {
-
+    
+    hour.min = "13:00";   // min and max opening times set for each button click
+    hour.max = "22:00";
+     document.getElementById("reserve_name").value = restName[0] + locations[0][4];
      document.getElementById("booking-title").textContent = restName[0] + locations[0][4];
      document.getElementById("times").textContent = open_Times[0] + locations[0][3] + open_Times[1] + locations[0][5] + open_Times[2];
 
    } else if (elem.id == "lexDeux") {
-       
-     document.getElementById("booking-title").textContent = restName[0] + locations[1][4];
-     document.getElementById("times").textContent = open_Times[0] + locations[1][3] + open_Times[1] + locations[1][5] + open_Times[2];
+
+    hour.min = "14:00";
+    hour.max = "20:00";
+    document.getElementById("reserve_name").value = restName[0] + locations[1][4];
+    document.getElementById("booking-title").textContent = restName[0] + locations[1][4];
+    document.getElementById("times").textContent = open_Times[0] + locations[1][3] + open_Times[1] + locations[1][5] + open_Times[2];
 
    }else if (elem.id == "knight") {
-
+     
+    hour.min = "13:00";
+    hour.max = "22:00";
+     document.getElementById("reserve_name").value = restName[0] + locations[2][4];
      document.getElementById("booking-title").textContent = restName[0] + locations[2][4];
+     document.getElementById("times").textContent = open_Times[0] + locations[2][3] + open_Times[1] + locations[2][5] + open_Times[2];
 
    }else if (elem.id == "bk-star") {
 
+    hour.min = "12:00";
+    hour.max = "22:00";
+     document.getElementById("reserve_name").value = restName[0] + locations[3][4];
      document.getElementById("booking-title").textContent = restName[0] + locations[3][4];
+     document.getElementById("times").textContent = open_Times[0] + locations[3][3] + open_Times[1] + locations[3][5] + open_Times[2];
 
    }else if (elem.id == "el-porto") {
 
+    hour.min = "12:00";
+    hour.max = "22:00";
+     document.getElementById("reserve_name").value = restName[0] + locations[4][4];
      document.getElementById("booking-title").textContent = restName[0] + locations[4][4];
+     document.getElementById("times").textContent = open_Times[0] + locations[4][3] + open_Times[1] + locations[4][5] + open_Times[2];
 
    }else if (elem.id == "lil-italy") {
 
+    hour.min = "14:00";
+    hour.max = "22:00";
+     document.getElementById("reserve_name").value = restName[0] + locations[5][4];
      document.getElementById("booking-title").textContent = restName[0] + locations[5][4];
+     document.getElementById("times").textContent = open_Times[0] + locations[5][3] + open_Times[1] + locations[5][5] + open_Times[2];
 
-   };
- };
-
-  
+   }
+ }
