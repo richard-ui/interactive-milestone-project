0c207a9e563f540abcb32250587bf1716cda6a3d
richard-ui <richard-jones96@hotmail.co.uk>
2021-01-20 17:15:04 +0000

    fix: rework code into multiple functions to validate form, in the send email js file to make it more readable and clear

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,40 +1,51 @@
-
-function sendMail(contactForm) {
-
-    // empty all form input fields to ""
+function clearInputs(){
+    
+    // empty all form input validation labels
 
     $("#f_name_label").html(""), $("#l_name_label").html(""), $("#phone_label").html(""),
     $("#email_label").html(""), $("#date_label").html(""), $("#hour_label").html("");
+    
+    // remove any red border's from input boxes by removing the 'red' class from each id
 
     document.getElementById('f_name').classList.remove("red"), document.getElementById('l_name').classList.remove("red"),
     document.getElementById('phone').classList.remove("red"), document.getElementById('email').classList.remove("red"),
     document.getElementById('date').classList.remove("red"), document.getElementById('hour').classList.remove("red");
 
+}
+
+// function to validate input fields
+
+function validateInput(input_name, validation_message){
+    if (!$('#' + input_name).val()) {    // use of jQuery for validation
+        document.getElementById(input_name).classList.add("red");
+        $("#"+input_name+"_label").html(validation_message);  // output
+      return false;
+    }
+    return true;
+
+}
+
+function sendMail(contactForm) {
+
+    // empty all form input fields to ""
+
+    clearInputs();
+
     /* Credits: Here is code to check whether input fields are empty from techiedelight.com */
-   
-    if (!$('#f_name').val()) {    // use of jQuery for validation
-        document.getElementById('f_name').classList.add("red");
-      $("#f_name_label").html("Please Enter your First Name!");  // output
-    }
-    else if (!$('#l_name').val()) {
-        document.getElementById('l_name').classList.add("red");
-      $("#l_name_label").html("Please Enter your Last Name!");
-    }
-    else if (!$('#phone').val()) {
-        document.getElementById('phone').classList.add("red");
-      $("#phone_label").html("Please Enter your Phone Number!");
-    }
-    else if (!$('#email').val()) {
-        document.getElementById('email').classList.add("red");
-      $("#email_label").html("Please Enter your E-mail!");
-    }
-    else if (!$('#date').val()) {
-        document.getElementById('date').classList.add("red");
-      $("#date_label").html("Please Enter a Date!");
-    }
-    else if (!$('#hour').val()) {
-        document.getElementById('hour').classList.add("red");
-      $("#hour_label").html("Please Enter a Time!");
+    
+   // validate function used in if statements, retrieving 2 parameters, 'input 'id' and display an error message in label
+
+    if (validateInput('f_name', "Please Enter your First Name!")){
+        if (validateInput('l_name', "Please Enter your Last Name!")){
+            if (validateInput('phone', "Please Enter your Phone Number!")){
+                if (validateInput('email', "Please Enter your E-mail!")){
+                    if (validateInput('date', "Please Enter a Date!")){
+                        if (validateInput('hour', "Please Enter a Time!")){
+                        }
+                    }
+                }
+            }
+        }
     }
     else{
     emailjs.send("service_csslju6", "Rick", {          // grab values of contact form input fields and send to 'Ricks' Email
