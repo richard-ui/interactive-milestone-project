7042c5f467e5907ff7c6127e44ab6e8def444eba
richard-ui <richard-jones96@hotmail.co.uk>
2021-01-21 16:33:10 +0000

    fix: Make function that is required to send email and call it when the user presses the submit button on the form. This new function allows for more clear readability and shows confirmation of there booking

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -16,7 +16,7 @@ function clearInputs(){
 // function to validate input fields
 
 function validateInput(input_name, validation_message){
-    if (!$('#' + input_name).val()) {    // use of jQuery for validation
+    if (!$('#'+input_name).val()) {    // use of jQuery for validation
         document.getElementById(input_name).classList.add("red");
         $("#"+input_name+"_label").html(validation_message);  // output
       return false;
@@ -33,42 +33,56 @@ function sendMail(contactForm) {
 
     /* Credits: Here is code to check whether input fields are empty from techiedelight.com */
     
-   // validate function used in if statements, retrieving 2 parameters, 'input 'id' and display an error message in label
+   // validate function used in if statements, retrieving 2 parameters, input 'id' and display an error message in label
 
-    if (validateInput('f_name', "Please Enter your First Name!")){
-        if (validateInput('l_name', "Please Enter your Last Name!")){
+     if (validateInput('f_name', "Please Enter your First Name")){
+        if (validateInput('l_name', "Please Enter your Last Name")){
             if (validateInput('phone', "Please Enter your Phone Number!")){
-                if (validateInput('email', "Please Enter your E-mail!")){
-                    if (validateInput('date', "Please Enter a Date!")){
-                        if (validateInput('hour', "Please Enter a Time!")){
+			    if (validateInput('email', "Please Enter your Email!")){
+			        if (validateInput('date', "Please Enter a Date!")){
+			            if (validateInput('hour', "Please Enter a Time!")){
+			             
                         }
+						
+                        else{
+						    emailjs.send("service_csslju6", "Rick", {          // grab values of contact form input fields and send to 'Ricks' Email
+								"reserve_name": contactForm.reserve_name.value, 
+								"first_name": contactForm.f_name.value,
+								"last_name": contactForm.l_name.value,
+								"phone": contactForm.phone.value,
+								"email": contactForm.email.value,
+								"date": contactForm.date.value,
+								"hour": contactForm.hour.value,
+								"guests": contactForm.guests.value,
+								"specialrequests": contactForm.special.value
+							})
+							emailjs.send("service_csslju6", "SendToUser", {          // grab values of contact form input fields and send to 'Ricks' Email
+								"email": contactForm.email.value,
+								"first_name": contactForm.f_name.value
+							})
+							.then(
+								function(response) {
+									console.log("SUCCESS", response);
+									$("#success-form").html("E-mail has been sent to the Developer!");    // message will output on the page if successful
+								},
+								function(error) {
+									console.log("FAILED", error);
+									$("#success-form").html("Something went wrong... E-mail Error!");  // error message will appear in label if there are any errors submitting form
+								}
+							);
+						}
+
+			
                     }
+			
                 }
+			
             }
         }
+
+
     }
-    else{
-    emailjs.send("service_csslju6", "Rick", {          // grab values of contact form input fields and send to 'Ricks' Email
-        "reserve_name": contactForm.reserve_name.value, 
-        "first_name": contactForm.f_name.value,
-        "last_name": contactForm.l_name.value,
-        "phone": contactForm.phone.value,
-        "email": contactForm.email.value,
-        "date": contactForm.date.value,
-        "hour": contactForm.hour.value,
-        "guests": contactForm.guests.value,
-        "specialrequests": contactForm.special.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            $("#success-form").html("E-mail has been sent to the Developer!");    // message will output on the page if successful
-        },
-        function(error) {
-            console.log("FAILED", error);
-            $("#success-form").html("Something went wrong... E-mail Error!");  // error message will appear in label if there are any errors submitting form
-        }
-    );
-    }
+
     return false;  // To block from loading a new page
+    
 }
